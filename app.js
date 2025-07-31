6b410fbaca914ccefdeec0f8519d5246c0d8b8d6
kaanbsaglam <kaanberk2009@hotmail.com>
2025-07-31 21:46:56 +0300

    purposefully throw error in health endpoint for testing purposes

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log('Health check endpoint hit!')
+
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
