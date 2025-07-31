02895bec61ff5a6f1222aa7331b842a90da3996c
kaanbsaglam <kaanberk2009@hotmail.com>
2025-07-31 22:05:04 +0300

    fix the error in the health endpoint

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ app.use(express.static('dist'))
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-console
   console.log('Health check endpoint hit!')
-
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
